829048d4018bfdbf3e5074b6ef165d200064e27c
RileyPBall <54775361+RileyPBall@users.noreply.github.com>
2019-09-10 23:20:20 -0700

    Add files via upload

---
+++ dice1.py
@@ -0,0 +1,16 @@
+from random import choices
+ntrials = 10000
+player1wins = 0
+player2dice = 2
+player1dice = 3
+for i in range(ntrials):
+    dice2=choices(range(1,7), k=player2dice)
+    total2 = dice2[0]+dice2[1]
+    if dice2[0]==dice2[1]:
+        continue
+    dice1=choices(range(1,7), k=player1dice)
+    dice1.sort(reverse=True)
+    total1= dice1[0]+dice1[1]
+    if total1 > total2:
+        player1wins+=1
+print("ratio of player 1 wins=", player1wins/ntrials)
